da3fb866d129e9e7a7d06fe66d734fdd292f9b70
pigu <ihcaoe@gmail.com>
2018-02-04 22:54:45 +0800

    ka7 liu5-ting5 sia2--tshut4-lai5

--- 教典詞性/匯入教典.py
+++ 教典詞性/匯入教典.py
@@ -13,73 +13,59 @@ from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 本調辭典.models import 本調辭典表
 
 
-
 def 匯入教典內的詞性():
     for 詞物件 in 教典資料.全部資料():
-        本調辭典表.objects.get_or_create(分詞=詞物件.轉音(臺灣閩南語羅馬字拼音).看分詞())
+        print(詞物件)
 
 
 class 教典資料:
     詞目總檔網址 = 'https://github.com/g0v/moedict-data-twblg/raw/master/uni/%E8%A9%9E%E7%9B%AE%E7%B8%BD%E6%AA%94.csv'
-    又音網址 = 'https://github.com/g0v/moedict-data-twblg/raw/master/uni/%E5%8F%88%E9%9F%B3.csv'
+    釋義網址 = 'https://github.com/g0v/moedict-data-twblg/raw/master/uni/釋義.csv'
+    詞性對照網址 = 'https://github.com/g0v/moedict-data-twblg/raw/master/uni/釋義.詞性對照.csv'
 
     @classmethod
     def 全部資料(cls):
         yield from cls._詞目總檔()
-        yield from cls._又見音表()
 
     @classmethod
     def _詞目總檔(cls):
+        釋義表 = cls._釋義表()
         with urlopen(cls.詞目總檔網址) as 檔:
             with io.StringIO(檔.read().decode()) as 資料:
                 for row in DictReader(資料):
+                    主編碼 = row['主編碼'].strip()
                     音讀 = row['音讀'].strip()
-                    if 音讀 == '':
-                        continue
                     漢字 = row['詞目'].strip()
                     for 一音 in 音讀.split('/'):
                         臺羅 = 一音.strip()
                         整理後漢字 = 文章粗胚.建立物件語句前處理減號(臺灣閩南語羅馬字拼音, 漢字)
                         整理後臺羅 = 文章粗胚.建立物件語句前處理減號(臺灣閩南語羅馬字拼音, 臺羅)
-                        try:
-                            for 詞物件 in (
-                                拆文分析器
-                                .對齊組物件(整理後漢字, 整理後臺羅)
-                                .網出詞物件()
-                            ):
-                                yield 詞物件
-                        except Exception as 錯誤:
-                            print(錯誤)
+                        for su5sing3 in 釋義表[主編碼]:
+                            yield 整理後漢字, 整理後臺羅, su5sing3
 
     @classmethod
-    def _又見音表(cls):
+    def _釋義表(cls):
+        詞性對照表 = cls._詞性對照表()
         資料 = {}
-        with urlopen(cls.詞目總檔網址) as 檔:
+        with urlopen(cls.釋義網址) as 檔:
             with io.StringIO(檔.read().decode()) as 字串資料:
                 for row in DictReader(字串資料):
                     主編碼 = row['主編碼'].strip()
-                    漢字 = row['詞目'].strip()
-                    音讀 = row['音讀'].split('/')[0].strip()
-                    資料[主編碼] = (漢字, 音讀)
+                    詞性代號 = row['詞性代號'].strip()
+                    詞性 = 詞性對照表[詞性代號]
+                    try:
+                        資料[主編碼].append(詞性)
+                    except KeyError:
+                        資料[主編碼] = [詞性]
+        return 資料
 
-        with urlopen(cls.又音網址) as 檔:
+    @classmethod
+    def _詞性對照表(cls):
+        詞性對照 = {}
+        with urlopen(cls.詞性對照網址) as 檔:
             with io.StringIO(檔.read().decode()) as 字串資料:
                 for row in DictReader(字串資料):
-                    if row['又音類型(1.又唸作 2.俗唸作 3.合音唸作)'] == '3':
-                        continue
-                    主編碼 = row['主編碼'].strip()
-                    (優勢腔漢字, _優勢腔音讀) = 資料[主編碼]
-                    優勢腔整理後漢字 = 文章粗胚.建立物件語句前處理減號(臺灣閩南語羅馬字拼音, 優勢腔漢字)
-                    for 一音 in row['又音'].split('/'):
-                        臺羅 = 一音.strip()
-                        整理後臺羅 = 文章粗胚.建立物件語句前處理減號(臺灣閩南語羅馬字拼音, 臺羅)
-                        try:
-                            for 詞物件 in (
-                                拆文分析器
-                                .對齊組物件(優勢腔整理後漢字, 整理後臺羅)
-                                .網出詞物件()
-                            ):
-                                yield 詞物件
-                        except Exception as 錯誤:
-                            print(錯誤)
-
+                    詞性代號 = row['詞性代號'].strip()
+                    詞性 = row['正確'].strip()
+                    詞性對照[詞性代號] = 詞性
+        return 詞性對照
