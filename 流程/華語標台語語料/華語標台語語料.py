94af125e2c0c50f5336ffc7f99fa66b0328692b2
薛丞宏 <ihcaoe@gmail.com>
2018-04-27 07:57:58 +0800

    得著json矣

--- 流程/華語標台語語料/華語標台語語料.py
+++ 流程/華語標台語語料/華語標台語語料.py
@@ -1,10 +1,22 @@
 from 提著詞性結果.views import 物件查國教院詞性
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+import json
 import os
+from sys import stdin
+
 
 if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "設定.settings")
-    結果 = 物件查國教院詞性(
-        拆文分析器.分詞句物件('逐-家｜tak8-ke1 做-伙｜tso3-hue2 來-𨑨-迌｜lai5-tshit4-tho5 ！'),
-    )
-    print(結果)
+    全部資料=[]
+    for 一句 in stdin.readlines():
+        句物件 = 拆文分析器.分詞句物件(一句.rstrip())
+        國教院詞性, _國教院詞條, _華語句 = 物件查國教院詞性(句物件)
+        句資料 = []
+        for 詞物件, 詞性 in zip(句物件.網出詞物件(), 國教院詞性):
+            句資料.append({
+                '漢字': 詞物件.看型(),
+                '臺羅': 詞物件.看音(),
+                '詞性': 詞性,
+            })
+        全部資料.append(句資料)
+    print(json.dumps(全部資料, ensure_ascii=False, sort_keys=True, indent=2))
