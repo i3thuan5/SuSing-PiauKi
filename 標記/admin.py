42e4c5422c09d4811a6e6ab93387278f7d430fbd
fafoy <ihcaoe@gmail.com>
2018-05-22 16:25:37 +0800

    ka github網站連結

--- 標記/admin.py
+++ 標記/admin.py
@@ -4,7 +4,12 @@ from 標記.管理.詞性標記 import 標記表
 from 標記.管理.詞性標記 import 標記表管理
 
 
+admin.site.site_header = '詞性標記系統'
+admin.site.site_title = '詞性標記系統'
+admin.site.site_url = 'https://github.com/i3thuan5/su5-sing3'
+
 admin.site.disable_action('delete_selected')
+
 
 class 語料狀況表管理(admin.ModelAdmin):
     list_display = ['id', '狀況', '確定會當用', ]
