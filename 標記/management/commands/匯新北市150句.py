97baf5a362877e754352a5496c284e6b32dbd51d
薛丞宏 <ihcaoe@gmail.com>
2018-05-25 12:00:31 +0800

    提示資訊

--- 標記/management/commands/匯新北市150句.py
+++ 標記/management/commands/匯新北市150句.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen
 
 from django.core.management.base import BaseCommand
 from django.db.transaction import atomic
+from django.db.utils import IntegrityError
 
 
 from 標記.models import 語料表
@@ -22,8 +23,12 @@ class Command(BaseCommand):
                 資料 = json.loads(檔.read().decode())
             for 編號, 內容 in sorted(資料.items()):
                 if int(編號) <= 150:
-                    語料表.objects.create(
-                        原本漢字=內容['例句漢字'], 原本羅馬字=內容['例句臺羅']
-                    )
+                    try:
+                        語料表.objects.create(
+                            原本漢字=內容['例句漢字'], 原本羅馬字=內容['例句臺羅']
+                        )
+                    except IntegrityError:
+                        print('可能匯過矣！', file=self.stdout)
+                        raise
 
         print('語料表數量：{}'.format(語料表.objects.count()), file=self.stdout)
