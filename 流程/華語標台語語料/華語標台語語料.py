76338ee301bc0c4f40967bed2c39e46814ff1d65
薛丞宏 <ihcaoe@gmail.com>
2018-06-05 16:27:12 +0800

    長句愛斷開

--- 流程/華語標台語語料/華語標台語語料.py
+++ 流程/華語標台語語料/華語標台語語料.py
@@ -9,20 +9,20 @@ if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "設定.settings")
     全部資料 = []
     for 一句 in stdin.readlines():
-        句物件 = 拆文分析器.分詞句物件(一句.rstrip())
-        try:
-            國教院詞性, _國教院詞條, _華語句 = 物件查國教院詞性(句物件)
-        except:
-            pass
-        else:
-          句資料 = []
-          for 詞物件, 詞性 in zip(句物件.網出詞物件(), 國教院詞性):
-            句資料.append({
-                '漢字': 詞物件.看型(),
-                '臺羅': 詞物件.看音(),
-                '詞性': 詞性,
-            })
-          全部資料.append(句資料)
+        for 句物件 in 拆文分析器.分詞章物件(一句.rstrip()).內底句:
+            try:
+                國教院詞性, _國教院詞條, _華語句 = 物件查國教院詞性(句物件)
+            except:
+                pass
+            else:
+                句資料 = []
+                for 詞物件, 詞性 in zip(句物件.網出詞物件(), 國教院詞性):
+                    句資料.append({
+                        '漢字': 詞物件.看型(),
+                        '臺羅': 詞物件.看音(),
+                        '詞性': 詞性,
+                    })
+                全部資料.append(句資料)
     print(
         json.dumps(全部資料, ensure_ascii=False, sort_keys=True, indent=2)
     )
