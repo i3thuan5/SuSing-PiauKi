cbbd1867a32a5c43a1b2ac936aed1d2fdd6c5242
薛丞宏 <ihcaoe@gmail.com>
2018-06-06 16:45:43 +0800

    先共仝款的句先提掉

--- 標記/management/commands/匯詞彙分級.py
+++ 標記/management/commands/匯詞彙分級.py
@@ -26,20 +26,28 @@ class Command(BaseCommand):
 
         c = HTTPSConnection(self.su5lui7)
         c.request("GET", self.tsu1liau7)
-        資料 = json.loads(c.getresponse().decode().read())
+        資料 = json.loads(c.getresponse().read().decode())
+        愛匯的資料 = []
+        有出現的資料 = set()
+        for 內容 in 資料['資料']:
+            for 一句 in 內容['文章資料']:
+                if '分詞' in 一句 and 一句['漢字'].strip():
+                    這句 = (一句['漢字'], 一句['臺羅'])
+                    if 這句 not in 有出現的資料:
+                        愛匯的資料.append(這句)
+                        有出現的資料.add(這句)
         with atomic():
-            for 內容 in 資料['資料']:
-                for 一句 in 內容['文章資料']:
-                    try:
-                        if '分詞' in 一句:
-                            語料表.objects.create(
-                                原本漢字=一句['漢字'], 原本羅馬字=內容['臺羅']
-                            )
-                    except IntegrityError:
-                        print(
-                            '{} {} 可能匯過矣！'.format(一句['漢字'], 內容['臺羅']),
-                            file=self.stdout
-                        )
-                        raise
+            for 這句 in 愛匯的資料:
+                漢, 羅 = 這句
+                try:
+                    語料表.objects.create(
+                        原本漢字=漢, 原本羅馬字=羅
+                    )
+                except IntegrityError:
+                    print(
+                        '{} {} 可能匯過矣！'.format(一句['漢字'], 一句['臺羅']),
+                        file=self.stdout
+                    )
+                    raise
 
         print('語料表數量：{}'.format(語料表.objects.count()), file=self.stdout)
