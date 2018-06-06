cafeb07b3ab53f67221eed7e226b186d7a39507d
薛丞宏 <ihcaoe@gmail.com>
2018-06-06 12:08:13 +0800

    整理程式

--- 流程/華語標台語語料/華語標台語語料.py
+++ 流程/華語標台語語料/華語標台語語料.py
@@ -1,13 +1,11 @@
 from 提著詞性結果.views import 物件查國教院詞性
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-import json
 import os
 from sys import stdin, stderr
 
 
-if __name__ == '__main__':
+def _main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "設定.settings")
-    全部資料 = []
     for 第幾句, 一句 in enumerate(stdin.readlines()):
         if 第幾句 % 100 == 0:
             print('做第 {} 句'.format(第幾句), file=stderr)
@@ -21,3 +19,7 @@ if __name__ == '__main__':
                 for 詞物件, 詞性 in zip(句物件.網出詞物件(), 國教院詞性):
                     詞性序列.append('{}/{}'.format(詞物件.看分詞(), 詞性))
                 print(' '.join(詞性序列))
+
+
+if __name__ == '__main__':
+    _main()
