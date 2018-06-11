94072c2ad96a7769b6b3027290624ae3b63fe6c5
薛丞宏 <ihcaoe@gmail.com>
2018-06-11 17:39:37 +0800

    更新compose
    修後臺介面
    
    裝上新的工具

--- 標記/admin.py
+++ 標記/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from 標記.models import 語料狀況表
 from 標記.管理.詞性標記 import 標記表
 from 標記.管理.詞性標記 import 標記表管理
+from 標記.管理.基礎句選擇 import 基礎句選擇表
+from 標記.管理.基礎句選擇 import 基礎句選擇管理
 
 
 admin.site.site_header = '詞性標記系統'
@@ -18,3 +20,4 @@ class 語料狀況表管理(admin.ModelAdmin):
 
 admin.site.register(語料狀況表, 語料狀況表管理)
 admin.site.register(標記表, 標記表管理)
+admin.site.register(基礎句選擇表, 基礎句選擇管理)
---
+++ 標記/管理/基礎句選擇.py
@@ -0,0 +1,45 @@
+
+from django.contrib import admin
+
+
+from 標記.models import 語料表
+from 標記.管理.ReadOnlyAdminFields import ReadOnlyAdminFields
+
+
+class 基礎句選擇表(語料表):
+
+    class Meta:
+        proxy = True
+        verbose_name = "基礎句選擇"
+        verbose_name_plural = verbose_name
+
+
+class 基礎句選擇管理(ReadOnlyAdminFields, admin.ModelAdmin):
+    # change list
+    list_display = [
+        'id',
+        '漢字', '羅馬字',
+        '備註',
+        'perplexity',
+    ]
+    ordering = ['-perplexity', 'id', ]
+    list_filter = ['語料狀況', ]
+    readonly_fields = ['漢字', '羅馬字', ]
+    search_fields = [
+        'id', '漢字', '羅馬字',
+    ]
+    list_per_page = 20
+
+    def has_add_permission(self, request):
+        # 薛：只能由程式上傳音檔和語料
+        # 薛：任何人都不能從後台新增
+        return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    class Media:
+        css = {
+            "all": ("css/admin_gi2_liau7_pio2.css", "css/moedictFont.css")
+        }
