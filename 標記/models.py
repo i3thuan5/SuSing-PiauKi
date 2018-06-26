dbf50be68f2dde8d9ed4774236d07226937ab7d6
薛丞宏 <ihcaoe@gmail.com>
2018-06-26 11:31:51 +0800

    改model

--- 標記/models.py
+++ 標記/models.py
@@ -9,7 +9,8 @@ class 語料表(models.Model):
     漢字 = models.TextField()
     羅馬字 = models.TextField()
     詞性 = models.TextField(blank=True)
-    perplexity = models.FloatField(default=-99)
+    perplexity = models.FloatField(default=99999)
+    來源 = models.TextField()
 
     備註 = models.TextField(blank=True)
     語料狀況 = models.ManyToManyField('語料狀況表', blank=True)
